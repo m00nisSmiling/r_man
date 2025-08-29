1817e23af961d4b650f116466fb101a52b6cba43
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2025-08-30 02:30:11 +0700

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 import os
 
 print("Installing requirements !!.....")
-os.system("pip install requests")
-os.system("pip install termcolor")
+os.system("pip install requests --break-system-packages")
+os.system("pip install termcolor --break-system-packages")
+os.system("pip install bs4 --break-system-packages")
